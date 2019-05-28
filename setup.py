8c1eb98af163e451a398d087ffac60c0432ebcb2
takvaviyagitrepos <51107478+takvaviyagitrepos@users.noreply.github.com>
2019-05-28 11:25:16 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'kmlfile',         # How you named your package folder (MyLib)
   packages = ['kmlfile'],   # Chose the same as "name"
-  version = '0.2',      # Start with a small number and increase it with every change you make
+  version = '0.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'kmlfile package for easy import and operations',   # Give a short description about your library
   author = 'Takvaviya',                   # Type in your name
   author_email = 'takvaviyagitrepos@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/takvaviyagitrepos',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/takvaviyagitrepos/kmlfile/archive/v_02.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/takvaviyagitrepos/kmlfile/archive/v_03.tar.gz',    # I explain this later on
   keywords = ['python', 'kml'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pykml',
